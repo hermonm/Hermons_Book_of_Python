0dd322192710059304a5b5cc0715af2b01ed0399
hermonm <hermonm@urbandaleschools.com>
2017-08-20 16:32:52 -0500

    Major changes for ch 18

--- charts.py
+++ charts.py
@@ -1,41 +1,77 @@
-#BAR CHART
+"""A program that encodes and decodes hidden messages in images through LSB steganography"""
+from PIL import Image, ImageFont, ImageDraw
+import textwrap
 
-'''Import pyplot library with plt abbreviation'''
+def decode_image(file_location="images/encoded_sample.png"):
+    """Decodes the hidden message in an image
+    file_location: the location of the image file to decode. By default is the provided encoded image in the images folder
+    """
+    encoded_image = Image.open(file_location)
+    red_channel = encoded_image.split()[0]
 
-import matplotlib.pyplot as plt
+    x_size = encoded_image.size[0]
+    y_size = encoded_image.size[1]
 
-'''Create some data series'''
+    decoded_image = Image.new("RGB", encoded_image.size)
+    pixels = decoded_image.load()
 
-x1 = [1, 2, 3, 4]
-y4 = [1, 3, 8, 4]
+    for i in range(x_size):
+        for j in range(y_size):
+            if bin(red_channel.getpixel((i, j)))[-1] == '0':
+                pixels[i, j] = (255, 255, 255)
+            else:
+                pixels[i, j] = (0,0,0)
+    decoded_image.save("images/decoded_image.png")
 
-'''Instead of using the numbers in the data series we can replace
-them with labels. Below we created a list of labels and then assigned
-them to the x values using 'xticks'. We could do the same thing with 
-the y values and use 'yticks'''
+def write_text(text_to_write, image_size):
+    """Writes text to an RGB image. Automatically line wraps
+    text_to_write: the text to write to the image
+    """
+    image_text = Image.new("RGB", image_size)
+    font = ImageFont.load_default().font
+    drawer = ImageDraw.Draw(image_text)
 
-labels = ['Test 1', 'Test 2', 'Test 3', 'Test 4']
-plt.xticks(x1, labels)
+    #Text wrapping. Change parameters for different text formatting
+    margin = offset = 10
+    for line in textwrap.wrap(text_to_write, width=60):
+        drawer.text((margin,offset), line, font=font)
+        offset += 10
+    return image_text
 
-'''Here we made a my_color variable which contains a string of four colors 
-which we will use to fill each bar.'''
+def encode_image(text_to_encode, template_image="images/samoyed.jpg"):
+    """Encodes a text message into an image
+    text_to_encode: the text to encode into the template image
+    template_image: the image to use for encoding. An image is provided by default.
+    """
+    template_image = Image.open(template_image)
+    red_template = template_image.split()[0]
+    green_template = template_image.split()[1]
+    blue_template = template_image.split()[2]
 
-my_colors = 'rgby'  #red, green, blue, yellow
+    x_size = template_image.size[0]
+    y_size = template_image.size[1]
 
+    #text draw
+    image_text = write_text(text_to_encode, template_image.size)
+    bw_encode = image_text.convert('1')
 
-'''Instead of plt.plot we use plt.bar to make a bar graph. The first two
-arguments are again the x and y data series. The third argument is the bar color
-which we set equal to my_colors which was our string of colors. The fourth
-argument is the percentage with of the bar. If 1 is used the bars will run together.'''
+    #encode text into image
+    encoded_image = Image.new("RGB", (x_size, y_size))
+    pixels = encoded_image.load()
+    for i in range(x_size):
+        for j in range(y_size):
+            red_template_pix = bin(red_template.getpixel((i,j)))
+            old_pix = red_template.getpixel((i,j))
+            tencode_pix = bin(bw_encode.getpixel((i,j)))
 
-plt.bar(x1, y4, color=my_colors, width=.8)
+            if tencode_pix[-1] == '1':
+                red_template_pix = red_template_pix[:-1] + '1'
+            else:
+                red_template_pix = red_template_pix[:-1] + '0'
+            pixels[i, j] = (int(red_template_pix, 2), green_template.getpixel((i,j)), blue_template.getpixel((i,j)))
 
-'''We again set the following three attributes of our graph.'''
+    encoded_image.save("images/encoded_image.png")
 
-plt.ylabel('Students Proficient')
-plt.xlabel('Competencies')
-plt.title('My Amazing Bar Chart', loc='center', fontsize=20)
-
-'''Don't forget to SHOW your graph after all of your hard work!'''
-
-plt.show()
+if __name__ == '__main__':
+    decode_image()
+    encode_image("hello world")
